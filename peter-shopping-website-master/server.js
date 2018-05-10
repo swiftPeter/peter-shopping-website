a7ca0c3b234425e8c09fad6966f8535b947d7bfb
swiftPeter <1137867366@qq.com>
2018-05-10 17:48:48 +0800

    Add files via upload

---
+++ peter-shopping-website-master/server.js
@@ -0,0 +1,11 @@
+var express=require('express');
+var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
